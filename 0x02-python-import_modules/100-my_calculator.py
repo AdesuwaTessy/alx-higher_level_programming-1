a90e4598c262f10688351bd6fcd71aa10233f62f
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-01 09:55:48 -0800

    task 100 Advance task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-    from sys import argv
-    
-    argc = len(argv) - 1
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-    if argc != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif argv[2] == '+':
-        result = add(int(argv[1]), int(argv[3]))
-    elif argv[2] == '-':
-        result = sub(int(argv[1]), int(argv[3]))
-    elif argv[2] == '*':
-        result = mul(int(argv[1]), int(argv[3]))
-    elif argv[2] == '/':
-        result = div(int(argv[1]), int(argv[3]))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{:s} {:s} {:s} = {:s}" .format(argv[1], argv[2], argv[3], result)
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    result = add(int(argv[1]), int(argv[3]))
+elif argv[2] == '-':
+    result = sub(int(argv[1]), int(argv[3]))
+elif argv[2] == '*':
+    result = mul(int(argv[1]), int(argv[3]))
+elif argv[2] == '/':
+    result = div(int(argv[1]), int(argv[3]))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+
+print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
