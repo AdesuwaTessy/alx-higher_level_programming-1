e4460f79b22cac1d5e29561ff4c0408e93b5c538
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-01 06:41:16 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4 as hidden
+    import hidden_4
 
-    names = dir(hidden)
-
+    names = dir(hidden_4)
     for name in names:
         if name[0:2] != "__":
             print("{:d}" .format(name))
