85cd2ef263d933e9f166b370fe92ae7283d9d5b5
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-01 09:21:41 -0800

    task 100 advance task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
+    import sys
     from sys import argv
     
     argc = len(argv) - 1
 
     if argc != 3:
-        print("Usage: {:s} <a> <operator> <b>" .format(argv[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif argv[2] == '+':
         result = add(int(argv[1]), int(argv[3]))
