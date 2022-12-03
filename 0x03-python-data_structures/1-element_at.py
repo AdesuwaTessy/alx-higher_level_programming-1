159828c5a7a5ed1565a9655fb6a6403743991d67
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-03 05:38:09 -0800

    Data structure task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 
 def element_at(my_list, idx):
 
-        if idx < 0:
-            return
-        elif idx >= len(my_list):
-            return
-        else:
-            return(my_list[idx])
+    if idx < 0:
+        return
+    elif idx >= len(my_list):
+        return
+    else:
+        return(my_list[idx])
