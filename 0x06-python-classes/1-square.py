7f3217a8500accad1ac8be52b542c8e49e695b00
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-20 07:58:28 -0800

    Python classes task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-#1-square.py - Daniel Seyi
+# 1-square.py - Daniel Seyi
 
-'''  a class Square that defines a square. It also has a Private instance attribute: size and Instantiation with size (no type/value verification) '''
+'''  a class Square that defines a square. It also has a Private instance
+attribute: size and Instantiation with size (no type/value verification) '''
 
 
 class Square:
     __size = None
 
+
 ''' Creating an instance with size '''
+
+
 def __init__(self, size):
     '''Initializing the class Square, with argument of szie'''
 
