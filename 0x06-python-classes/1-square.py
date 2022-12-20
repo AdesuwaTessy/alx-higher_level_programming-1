1af9eb9b31b63f03f832d880a5345c73a75d02a0
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-20 08:00:04 -0800

    Python classes task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,13 +6,11 @@ attribute: size and Instantiation with size (no type/value verification) '''
 
 
 class Square:
-    __size = None
+    """Represents a square"""
 
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
-''' Creating an instance with size '''
-
-
-def __init__(self, size):
-    '''Initializing the class Square, with argument of szie'''
-
-    self.__size = size
+        self.__size = size
