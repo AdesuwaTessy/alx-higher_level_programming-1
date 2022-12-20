83ed21457477ced86db58a742907b326dff49bb2
SesanDaniel <obisesan.seyid@gmail.com>
2022-12-20 07:32:12 -0800

    Python Classes task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    An empty class Square that defines a square:
+'''
+
+
+class Square:
+    pass  # This line allows the script to pass class with empty block
+
+
+square = Square()
+print(square)
